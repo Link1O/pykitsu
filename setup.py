55634862d7238229a7a3d0b75db419f1a1663fe9
Ore <elhamradriss@gmail.com>
2024-01-14 05:29:55 +0100

    version notice change

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ packages = [
 ]
 setup(
     name='pykitsu',
-    version='0.3.0',
+    version='0.3.4',
     description='kitsu.io python api wrapper',
     long_description='an asynchronous and fast api wrapper for kitsu.io',
     author='Ore',
